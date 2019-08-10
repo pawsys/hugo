6645c9b985cf2ae1119b9d31e570487537481807
Pawel Sysiak <pawsys@gmail.com>
2019-08-09 18:03:18 -0700

    new tailwind setup

---
+++ tools/tailwind.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+  theme: {
+    fontFamily: {
+      sans: ['"Roboto"', '-apple-system', 'BlinkMacSystemFont', '"Segoe UI"', 'Roboto', '"Helvetica Neue"', 'Arial', '"Noto Sans"', 'sans-serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
+      serif: ['"IBM Plex Serif"', 'Georgia', 'Cambria', '"Times New Roman"', 'Times', 'serif', '"Apple Color Emoji"', '"Segoe UI Emoji"', '"Segoe UI Symbol"', '"Noto Color Emoji"',],
+    },
+    extend: {
+      colors: {
+        primary: '#777DDC',
+      }
+    }
+  },
+  variants: {},
+  plugins: []
+}
